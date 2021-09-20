92b009dc3bdf700f99f5f6d505cf62fa67b9401f
Ghost <67116890+Ghost1129@users.noreply.github.com>
2021-09-20 13:48:30 +0530

    Update _app.js
    Added user data from firebase

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,9 @@ import 'tailwindcss/tailwind.css'
 import {useAuthState} from 'react-firebase-hooks/auth'
 import { auth,db } from '../firebase'
 import Login from'./login'
-import Loading from '../components/Loading'
-import { useEffect } from 'react'
-import firebase from 'firebase'
+import Loading from '../components/Loading';
+import { useEffect } from 'react';
+import firebase from 'firebase';
 
 function MyApp({ Component, pageProps }) {
   const [user,loading] = useAuthState(auth);
