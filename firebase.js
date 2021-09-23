0cdc7bedb7e267885dd8c974352a085398540df4
“Ghost1129” <1129.gh0st@gmail.com>
2021-09-23 15:06:52 +0530

    Update

--- firebase.js
+++ firebase.js
@@ -1,14 +1,6 @@
 
 import firebase from 'firebase'
-
-const firebaseConfig = {
-    apiKey: "AIzaSyCxhxrlESqsb96xfPNlhIj5rHu5C2XKXbo",
-    authDomain: "whatsapp-db343.firebaseapp.com",
-    projectId: "whatsapp-db343",
-    storageBucket: "whatsapp-db343.appspot.com",
-    messagingSenderId: "827124765101",
-    appId: "1:827124765101:web:99c0763998afee72fdd124"
-  };
+import { firebaseConfig } from "./firebaseConfig"
 
   const app = !firebase.apps.length
   ? firebase.initializeApp(firebaseConfig)
---
+++ firebaseConfig.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCxhxrlESqsb96xfPNlhIj5rHu5C2XKXbo",
+    authDomain: "whatsapp-db343.firebaseapp.com",
+    projectId: "whatsapp-db343",
+    storageBucket: "whatsapp-db343.appspot.com",
+    messagingSenderId: "827124765101",
+    appId: "1:827124765101:web:99c0763998afee72fdd124"
+  };
