09a8721328f4ee3bac8d2ed3d63ae9f0f8de4975
Ghost <67116890+Ghost1129@users.noreply.github.com>
2021-09-23 14:37:36 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,7 +1,14 @@
 
 import firebase from 'firebase'
-import firebaseConfig from './firebaseAPI';
 
+  const firebaseConfig = {
+    apiKey: "AIzaSyCxhxrlESqsb96xfPNlhIj5rHu5C2XKXbo",
+    authDomain: "whatsapp-db343.firebaseapp.com",
+    projectId: "whatsapp-db343",
+    storageBucket: "whatsapp-db343.appspot.com",
+    messagingSenderId: "827124765101",
+    appId: "1:827124765101:web:99c0763998afee72fdd124"
+  };
   const app = !firebase.apps.length
   ? firebase.initializeApp(firebaseConfig)
   : firebase.app();
